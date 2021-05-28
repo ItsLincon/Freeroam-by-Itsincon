66a520af10c37a3b093837553117f7175a3886cd
Lincon <59184093+ItsLincon@users.noreply.github.com>
2021-05-28 16:12:13 +0200

    Scripts

---
+++ server-data/resources/[core]/commands/fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Delete Vehicle and load car script written by ItsLincon
+fx_version "bodacious"
+game "gta5"
+
+name "Commands"
+description "commands /dv and /car for FiveM stadlone servers by ItsLincon"
+author "ItsLincon"
+version "1.1.0"
+
+-- Add a client script 
+client_script "client.lua"
